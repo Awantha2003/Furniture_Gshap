3b0c487f721f2e1abc4198965e477783ac34823d
Awantha2003 <awanthaimesh65@gmail.com>
2025-12-29 19:41:32 +0530

    add producat card file only

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductCard = ({price,position,src}) => {
+  return (
+  <div className="relative overflow-clip saturate-120 bg-zinc-200 w-full cursor-pointer transition-all duration-200 
+    hover:-translate-y-1.5 ease-in lg:w-[95%] h-[]">
+
+    </div>
+  )
+}
+
+export default ProductCard
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -2,15 +2,28 @@ import React from 'react'
 
 function Trending() {
   return (
-    <section className="trending-section">
-      <div className="section-header">
+    <section className="max-w-7xl mx-auto p-4 sm:p-4 mt-20 mb-20">
+
+      {/* title */}
+      <div className="relative flex justify-between items-end mb-12 ">
         <h2 className="section-title">
           
          FOR TRENDING <br/> NOW
         </h2>
-         <button className=''>
+         <button className='text-sm lg:text-lg absolute right-0 font-medium border-zinc-900 text-zinc-900 px-5 py-3 rounded-full cursor-pointer hover:bg-zinc-900 hover:text-zinc-50 transition-all duration-200 ease-in'>
           SEE ALL
          </button>
+      </div>
+
+    {/* Trending items grid */}
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+
+        {/* Item 1 */}
+      <div className="col-span1">
+        
+      </div>
+       
+       {/* Item 2 & 3  */}
       </div>
 
     </section>
